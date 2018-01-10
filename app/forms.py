e686d1e48c88691eaf1a54a9f00da3e6cecddd0b
Nitzanel <nitzan@claysciences.com>
2018-01-10 11:44:57 +0200

    Interactive file reading

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,21 @@ from wtforms import IntegerField, StringField, PasswordField, SubmitField, FileF
 from wtforms.validators import DataRequired
 
 class UploadForm(FlaskForm):
-    gtf_file = FileField('Gtf file',validators=[DataRequired()])
-    specie_selection = SelectField('Specie',choices=conf.supported_species,validators=[DataRequired()])
-    submit = SubmitField('Upload',render_kw={'class':'btn btn-primary'})
+    gtf_file = FileField(
+        'Gtf file',
+        render_kw={
+            'id':'gtfUpload',
+            'onchange':'fileUploadCallback();'
+        },
+        validators=[DataRequired()]
+    )
+    specie_selection = SelectField(
+        'Specie',
+        choices=conf.supported_species,
+        validators=[DataRequired()]
+    )
+    submit = SubmitField(
+        'Upload',
+        render_kw={'class':'btn btn-primary'}
+    )
 
